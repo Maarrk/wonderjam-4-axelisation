2e1a7d8076e00c51fab44775ed0bdbaa43ee9778
Marek S. Łukasiewicz <marek@lukasiewicz.tech>
2023-03-05 17:58:13 +0100

    Basic movement

--- src/game.zig
+++ src/game.zig
@@ -1,37 +1,100 @@
 const std = @import("std");
+
 const rl = @import("raylib");
 
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = gpa.allocator();
 
+const Vector2 = rl.Vector2;
+const Axis = rl.GamepadAxis;
+const Key = rl.KeyboardKey;
+
 pub const Model = struct {
-    player_position: rl.Vector2 = rl.Vector2{
-        .x = 1.0,
-        .y = 2.0,
-    },
+    player_position: Vector2 = Vector2.zero(),
+    camera_position: Vector2 = Vector2.zero(),
 };
 
-pub const InputAction = union {
-    move_up: f32,
-    move_right: f32,
-    use: void,
+const InputTrigger = enum {
+    none,
+    press,
+    hold,
+    release,
 };
 
-pub fn input() std.ArrayList(InputAction) {
-    var list = std.ArrayList(InputAction).init(allocator);
-    return list;
+pub const InputActions = struct {
+    move_right: f32 = 0.0,
+    move_down: f32 = 0.0,
+    use: InputTrigger = InputTrigger.none,
+};
+
+pub fn input() InputActions {
+    var actions = InputActions{};
+
+    // Player movement
+    if (rl.IsGamepadAvailable(0)) {
+        const deadzone = 0.3;
+
+        const x_axis = rl.GetGamepadAxisMovement(0, Axis.GAMEPAD_AXIS_LEFT_X);
+        if (@fabs(x_axis) > deadzone) actions.move_right = x_axis;
+
+        const y_axis = rl.GetGamepadAxisMovement(0, Axis.GAMEPAD_AXIS_LEFT_Y);
+        if (@fabs(y_axis) > deadzone) actions.move_down = y_axis;
+    }
+    if (rl.IsKeyDown(Key.KEY_RIGHT)) {
+        actions.move_right += 1.0;
+    }
+    if (rl.IsKeyDown(Key.KEY_LEFT)) {
+        actions.move_right -= 1.0;
+    }
+    if (rl.IsKeyDown(Key.KEY_DOWN)) {
+        actions.move_down += 1.0;
+    }
+    if (rl.IsKeyDown(Key.KEY_UP)) {
+        actions.move_down -= 1.0;
+    }
+    actions.move_right = rl.Clamp(actions.move_right, -1.0, 1.0);
+    actions.move_down = rl.Clamp(actions.move_down, -1.0, 1.0);
+
+    // Button presses
+    actions.use = keyState(Key.KEY_SPACE);
+
+    return actions;
 }
 
-pub fn update(model: Model, input_actions: std.ArrayList(InputAction)) Model {
-    _ = input_actions;
-    return model;
+fn keyState(key: Key) InputTrigger {
+    if (rl.IsKeyPressed(key)) return InputTrigger.press;
+    if (rl.IsKeyReleased(key)) return InputTrigger.release;
+    if (rl.IsKeyDown(key)) return InputTrigger.hold;
+    return InputTrigger.none;
 }
 
-pub fn view(model: Model) !void {
+pub fn update(model: Model, input_actions: InputActions, delta_time: f64) Model {
+    var m = model;
+    const dt = @floatCast(f32, delta_time);
+
+    const player_velocity = 3.0;
+    m.player_position.x += input_actions.move_right * player_velocity * dt;
+    m.player_position.y += input_actions.move_down * player_velocity * dt;
+
+    return m;
+}
+
+pub fn view(model: Model, screen_size: Vector2) !void {
     rl.ClearBackground(rl.BLACK);
+
+    var camera: rl.Camera2D = rl.Camera2D{ .target = model.camera_position };
+    camera.offset = Vector2{ .x = screen_size.x / 2.0, .y = screen_size.y / 2.0 };
+    camera.zoom = 64.0;
+
+    {
+        rl.BeginMode2D(camera);
+        defer rl.EndMode2D();
+
+        rl.DrawCircleV(model.player_position, 0.5, rl.RED);
+    }
 
     const status_text = try std.fmt.allocPrint(allocator, "Player position: x:{d:.3} y:{d:.3}\x00", .{ model.player_position.x, model.player_position.y });
     defer allocator.free(status_text);
 
-    rl.DrawText(@ptrCast([*:0]const u8, status_text), 100, 100, 20, rl.YELLOW);
+    rl.DrawText(@ptrCast([*:0]const u8, status_text), 120, 10, 20, rl.YELLOW);
 }
--- src/main.zig
+++ src/main.zig
@@ -2,24 +2,31 @@ const rl = @import("raylib");
 const game = @import("game.zig");
 
 pub fn main() !void {
-    rl.InitWindow(800, 450, "Axelisation");
+    const width = 800;
+    const height = 450;
+    rl.InitWindow(width, height, "Axelisation");
+    const screen_size = rl.Vector2{ .x = width, .y = height };
     rl.SetWindowState(@enumToInt(rl.ConfigFlags.FLAG_WINDOW_RESIZABLE));
     rl.SetTargetFPS(60);
     defer rl.CloseWindow();
 
     var model = game.Model{};
+    var previous_time: f64 = rl.GetTime();
 
     while (!rl.WindowShouldClose()) {
+        const current_time: f64 = rl.GetTime();
+        const delta_time = current_time - previous_time;
+
         rl.BeginDrawing();
         defer rl.EndDrawing();
 
         const input_actions = game.input();
-        defer input_actions.deinit();
 
-        model = game.update(model, input_actions);
+        model = game.update(model, input_actions, delta_time);
 
-        try game.view(model);
+        try game.view(model, screen_size);
 
         rl.DrawFPS(10, 10);
+        previous_time = current_time;
     }
 }
